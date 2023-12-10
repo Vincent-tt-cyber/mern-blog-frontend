b9f72a6e4920fd43c07d1e23bb11abebad76bb66
Vincent_tt <evgeniy161102@gmail.com>
2023-12-10 20:48:33 +0300

    fix

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -134,7 +134,7 @@ export const AddPost = () => {
           </Button>
           <img
             className={styles.image}
-            src={`${process.env.REAC_APP_API_URL}${imageUrl}`}
+            src={`${process.env.REACT_APP_API_URL}${imageUrl}`}
             alt="Uploaded"
           />
         </>
