3cb21b3a86e30dc5d0604f5368fb5a3375ca5b6c
Vincent_tt <evgeniy161102@gmail.com>
2023-12-10 16:44:34 +0300

    fix

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -56,7 +56,7 @@ export const Post = ({
       {imageUrl && (
         <img
           className={clsx(styles.image, { [styles.imageFull]: isFullPost })}
-          src={imageUrl}
+          src={`${process.env.REAC_APP_API_URL}/${imageUrl}`}
           alt={title}
         />
       )}
