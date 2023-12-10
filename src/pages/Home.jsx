9d27e586411ec2f21d5c925246f785d1322f4245
Vincent_tt <evgeniy161102@gmail.com>
2023-12-10 20:57:29 +0300

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,7 +44,9 @@ export const Home = () => {
               <Post
                 _id={obj._id}
                 title={obj.title}
-                imageUrl={obj.imageUrl ? `${obj.imageUrl}` : ""}
+                imageUrl={
+                  obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}` : ""
+                }
                 user={obj.user}
                 createdAt={obj.createdAt}
                 viewsCount={obj.viewsCount}
